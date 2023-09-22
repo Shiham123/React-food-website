0e5eb46ef44c7b2236c2a7b28685f76400008039
Shiham123 <s36651786@gmail.com>
2023-09-22 10:14:11 +0600

    quick project

--- src/components/category.jsx
+++ src/components/category.jsx
@@ -1,5 +1,28 @@
+import { categories } from '../data/data.js';
+
 const Category = () => {
-  return <div></div>;
+  return (
+    <div className="max-w-[1640px] m-auto px-4 py-12">
+      <h1 className="text-orange-600 font-bold text-4xl text-center">
+        Top rated menu
+      </h1>
+      {/* Categories */}
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-6 py-6">
+        {categories.map((category, key) => {
+          const { image, name } = category;
+          return (
+            <div
+              key={key}
+              className="bg-gray-100 rounded-lg p-4 flex justify-between items-center"
+            >
+              <h2 className="font-bold sm:text-xl">{name}</h2>
+              <img className="w-20" src={image} alt={name} />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Category;
